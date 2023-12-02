e1c389c4c3b897b8d8f3bd8fd254d88b549ee510
dvvaa <127208090+dvvaa@users.noreply.github.com>
2023-12-02 16:45:12 +0100

    dddd

---
+++ assets/js/app.js
@@ -0,0 +1,9 @@
+/*
+Documento de JavaScript
+        Nombre: app.js
+        Autor:  Lydia Pérez
+        Fecha:  02/12/2023
+        Descripcion:  Archivo de JavaScript para la página web del portafolio
+*/
+
+//
